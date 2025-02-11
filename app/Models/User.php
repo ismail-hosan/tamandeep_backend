14a9a9297cd6ac8e499a0a26a102c5daf0a6fc8d
bdcalling <mdrobinhosan57@gmail.com>
2025-02-11 14:58:05 +0600

    change validation

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, Billable;
+    use HasApiTokens, HasFactory, Notifiable;
 
 
     protected $fillable = [
@@ -51,6 +51,11 @@ class User extends Authenticatable
     public function cart()
     {
         return $this->belongsTo(Cart::class,'user_id');
+    }
+
+    public function subscription()
+    {
+        return $this->belongsTo(Subscription::class);
     }
 
 
