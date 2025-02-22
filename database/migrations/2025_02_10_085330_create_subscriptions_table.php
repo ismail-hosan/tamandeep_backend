7167446656a30df7aa21305d94dcb9f648e746bd
bdcalling <mdrobinhosan57@gmail.com>
2025-02-22 11:34:17 +0600

    changes subcription database chanage

--- database/migrations/2025_02_10_085330_create_subscriptions_table.php
+++ database/migrations/2025_02_10_085330_create_subscriptions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,9 @@ return new class extends Migration
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('plan')->nullable();
+            $table->string('period_start')->nullable();
+            $table->string('period_end')->nullable();
             $table->string('stripe_subscription_id')->nullable();
             $table->string('status')->default('inactive');
             $table->timestamps();
